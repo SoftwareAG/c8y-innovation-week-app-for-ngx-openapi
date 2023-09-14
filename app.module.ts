0464e979bc825a6449f517dbc06a1de84030325d
pmic <pmic@softwareag.com>
2023-09-15 00:14:30 +0200

    Working version with displaying users

--- app.module.ts
+++ app.module.ts
@@ -4,6 +4,7 @@ import { RouterModule as ngRouterModule } from '@angular/router';
 import { CoreModule, BootstrapComponent, RouterModule, HOOK_COMPONENTS } from '@c8y/ngx-components';
 import { UsersComponent } from './users-component/users.component';
 import { UsersModule } from './users-component/users.module';
+import { ApiModule } from 'ngx-c8y-openapi-library';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { UsersModule } from './users-component/users.module';
     RouterModule.forRoot(),
     ngRouterModule.forRoot([], { enableTracing: false, useHash: true }),
     CoreModule.forRoot(),
-    UsersModule
+    UsersModule,
+    ApiModule.forRoot({ rootUrl: 'http://localhost:9000' }),
   ],
   declarations: [],
   bootstrap: [BootstrapComponent],
--- users-component/users.component.ts
+++ users-component/users.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { BreadcrumbService, CoreModule } from '@c8y/ngx-components';
+import { UsersService } from 'ngx-c8y-openapi-library';
+import { User } from 'ngx-c8y-openapi-library/api/models/user';
+import { UserCollection } from 'ngx-c8y-openapi-library/api/models/user-collection';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'c8y-users-component',
@@ -9,10 +13,15 @@ import { BreadcrumbService, CoreModule } from '@c8y/ngx-components';
 })
 @Injectable()
 export class UsersComponent implements OnInit {
- 
-  constructor(public breadcrumbService: BreadcrumbService) { }
-  ngOnInit() {
-    console.log('Hello users');
-  }
+ public users$: Observable<UserCollection>;
+  constructor(public breadcrumbService: BreadcrumbService,
+    public userService: UsersService) { }
 
+  async ngOnInit() {
+    console.log('Hello users');
+
+    this.users$ = this.userService.getUserCollectionResource({
+      tenantId: 't56293'
+    });
+  }
 }
